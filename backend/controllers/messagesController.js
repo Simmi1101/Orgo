f7b02e5ad4ad47de9dc0c78682ef8e7862c0d12e
Simmi <simmi.chaudhary11@gmail.com>
2024-11-08 10:41:16 +0530

    removed certain things

--- backend/controllers/messagesController.js
+++ backend/controllers/messagesController.js
@@ -1,67 +1,68 @@
 const { log } = require("util");
 const Messages = require("../models/messageModel");
-const generateFileUrl = require("../s3");
-const AWS = require("aws-sdk");
+// const generateFileUrl = require("../s3");
+// const AWS = require("aws-sdk");
 require("dotenv").config();
 
-const s3 = new AWS.S3({
-    accessKeyId: process.env.AWS_ACCESSKEYID,
-    secretAccessKey: process.env.AWS_SECRETACCESSKEY
-})
+const fs = require("fs");
+const path = require("path");
 
+// const s3 = new AWS.S3({
+//     accessKeyId: process.env.AWS_ACCESSKEYID,
+//     secretAccessKey: process.env.AWS_SECRETACCESSKEY
+// })
 
-const addMessage = async(req, res, next) => {
-    try {
-        
-        const {from, to, message} = req.body;
-        const data = await Messages.create({
-            message: {text: message},
-            users: [from, to],
-            sender: from,
-        });
-        
-        if(data){
-            return res.json({
-                msg:"Message added successfully."
-            })
-        }else{
-            return res.json({
-                success:true,
-                msg:"Failed to add message."
-            })
-        }
-    } catch (error) {
-        return res.json({
-            status:400,
-            msg: error.msg
-        })
+const addMessage = async (req, res, next) => {
+  try {
+    const { from, to, message } = req.body;
+    const data = await Messages.create({
+      message: { text: message },
+      users: [from, to],
+      sender: from,
+    });
+
+    if (data) {
+      return res.json({
+        msg: "Message added successfully.",
+      });
+    } else {
+      return res.json({
+        success: true,
+        msg: "Failed to add message.",
+      });
     }
-}
+  } catch (error) {
+    return res.json({
+      status: 400,
+      msg: error.msg,
+    });
+  }
+};
 
-const getallMessage = async(req, res, next) => {
-    try {
-        const { from, to } = req.body;
-    
-        const messages = await Messages.find({
-          users: {
-            $all: [from, to],
-          }
-        }).sort({ updatedAt: 1 });
-        
-        const projectedMessages = messages.map((msg) => {
-          return {
-            fromSelf: msg.sender.toString() === from,
-            message: msg.message.text
-          };
-        }); 
-        res.json(projectedMessages);
-    }catch(error){
-        return res.json({
-            status:400,
-            msg: error.msg
-        })
-    }
-}
+const getallMessage = async (req, res, next) => {
+  try {
+    const { from, to } = req.body;
+
+    const messages = await Messages.find({
+      users: {
+        $all: [from, to],
+      },
+    }).sort({ updatedAt: 1 });
+
+    const projectedMessages = messages.map((msg) => {
+      return {
+        fromSelf: msg.sender.toString() === from,
+        message: msg.message.text,
+      };
+    });
+    res.json(projectedMessages);
+  } catch (error) {
+    return res.json({
+      status: 400,
+      msg: error.msg,
+    });
+  }
+};
 
 // const uploadFile = async(req, res) => {
 //     console.log("result",req.file);
@@ -73,127 +74,190 @@ const getallMessage = async(req, res, next) => {
 //         const newFile = new Messages(file)
 
 //         const savefile = newFile.save()
-    
+
 //         res.send({status:"ok", data:savefile})
-        
+
 //     }catch(error){
 //         res.json({status:error})
 //     }
 // }
-    
 
-const getMsgHistory = async(req, res, next) => {
-    try {
-        const allMessages = await Messages.find({})
-            
-        // console.log("allMessages",allMessages);
-        
-        return res.json({
-            status:200,
-            data:allMessages
-        })
-    } catch (error) {
-        return res.json({
-            status:400,
-            msg: error.message
-        })
+const getMsgHistory = async (req, res, next) => {
+  try {
+    const allMessages = await Messages.find({});
+
+    // console.log("allMessages",allMessages);
+
+    return res.json({
+      status: 200,
+      data: allMessages,
+    });
+  } catch (error) {
+    return res.json({
+      status: 400,
+      msg: error.message,
+    });
+  }
+};
+const getUploadFile = async (req, res) => {
+  try {
+    const uploadFiles = req.files; // Assume req.files contains uploaded file data
+    const { from, to } = req.body;
+
+    // Define the upload directory
+    const uploadDir = path.join(__dirname, "..", "uploads");
+
+    // Ensure the upload directory exists
+    if (!fs.existsSync(uploadDir)) {
+      fs.mkdirSync(uploadDir);
     }
-}
 
-const getUploadFile = async(req, res) => {
-    try {
-        const uploadFiles = req.files;
-        const {from, to} = req.body;
-      
-        const awsuploads = await uploadFileOnly(uploadFiles.files.data, uploadFiles.files.name, "");
-        
-        const data = new Messages({
-          message: {text: awsuploads.data },
-          users: [from, to],
-          sender: from,
-        });
+    // Define the file path
+    const filePath = path.join(uploadDir, uploadFiles.files.name);
 
-        await data.save();
-        
-        return res.status(200).json({
-            status:200,
-            msg:"file uploaded",
-            data: data
-            }); 
+    // Save the file locally
+    fs.writeFileSync(filePath, uploadFiles.files.data);
 
-    } catch (error) {
-        return res.status(400).json({
-            status:400,
-            message: error.message
-        })
-    }
-}
+    // Create the URL to access the file
+    const fileUrl = `/uploads/${uploadFiles.files.name}`;
 
-const getUploadFileUrl = async(req, res) => {
-    try{
-        const fileUrl = req.body.file;
-        console.log("fileUrl", fileUrl)
-        const getFile = await get_secure_url(fileUrl);
+    // Save file URL in the message (similar to how you did with S3)
+    const data = new Messages({
+      message: { text: fileUrl },
+      users: [from, to],
+      sender: from,
+    });
 
-        return res.status(200).json({
-            status:200,
-            data:getFile
-        })
-    }catch(error){
-        return res.json({
-            status:400,
-            msg: error.message
-        })
-    }
-}
+    await data.save();
+
+    return res.status(200).json({
+      status: 200,
+      msg: "File uploaded successfully",
+      data: data,
+    });
+  } catch (error) {
+    return res.status(400).json({
+      status: 400,
+      message: error.message,
+    });
+  }
+};
+
+// const getUploadFile = async(req, res) => {
+//     try {
+//         const uploadFiles = req.files;
+//         const {from, to} = req.body;
+
+//         const awsuploads = await uploadFileOnly(uploadFiles.files.data, uploadFiles.files.name, "");
+
+//         const data = new Messages({
+//           message: {text: awsuploads.data },
+//           users: [from, to],
+//           sender: from,
+//         });
+
+//         await data.save();
+
+//         return res.status(200).json({
+//             status:200,
+//             msg:"file uploaded",
+//             data: data
+//             });
+
+//     } catch (error) {
+//         return res.status(400).json({
+//             status:400,
+//             message: error.message
+//         })
+//     }
+// }
+
+const getUploadFileUrl = async (req, res) => {
+  try {
+    const fileUrl = req.body.file;
+
+    return res.status(200).json({
+      status: 200,
+      data: fileUrl,
+    });
+  } catch (error) {
+    return res.status(400).json({
+      status: 400,
+      msg: error.message,
+    });
+  }
+};
+
+// const getUploadFileUrl = async(req, res) => {
+//     try{
+//         const fileUrl = req.body.file;
+//         console.log("fileUrl", fileUrl)
+//         const getFile = await get_secure_url(fileUrl);
+
+//         return res.status(200).json({
+//             status:200,
+//             data:getFile
+//         })
+//     }catch(error){
+//         return res.json({
+//             status:400,
+//             msg: error.message
+//         })
+//     }
+// }
 
 const uploadFileOnly = (fileContent, fileName, ContentEncoding) => {
-    // Read content from the file
-    //const fileContent = fs.readFileSync(fileName);
-  
-    // Setting up S3 upload parameters
-    var params = {
-      Bucket: process.env.AWS_BUCKETNAME,
-      Key: fileName, // File name you want to save as in S3
-      Body: fileContent,
-      //ACL:'public-read'
-    };
-  
-    if (ContentEncoding) {
-      params.ContentEncoding = ContentEncoding;
-    }
-  
-    // Uploading files to the bucket
-    return new Promise((resolve, reject) => {
-      s3.upload(params, function (err, data) {
-        if (err) {
-          resolve({
-            statusCode: 500,
-            error: err,
-          });
-          return;
-        }
-        resolve({
-          statusCode: 200,
-          data: data.Location,
-        });
-        console.log(`File uploaded successfully. ${data.Location}`);
-      });
-    });
+  // Read content from the file
+  //const fileContent = fs.readFileSync(fileName);
+
+  // Setting up S3 upload parameters
+  var params = {
+    Bucket: process.env.AWS_BUCKETNAME,
+    Key: fileName, // File name you want to save as in S3
+    Body: fileContent,
+    //ACL:'public-read'
   };
 
+  if (ContentEncoding) {
+    params.ContentEncoding = ContentEncoding;
+  }
 
-  // const get_secure_url = (fileName) => {
-  //   return new Promise((resolve, reject) => {
-  //     s3.getSignedUrl('getObject', {
-  //       Bucket: process.env.AWS_BUCKETNAME,
-  //       Key: fileName.indexOf('s3.amazonaws.com') > -1 ? encodeURI(fileName.split('.s3.amazonaws.com/')[1]) : fileName,
-  //       Expires: 60 * 5, // time in seconds: e.g. 60 * 5 = 5 mins
-  //     }, (err, url) => {
-  //       if (err) throw err;
-  //       resolve(url);
-  //     });
-  //   });
-  // }
+  // Uploading files to the bucket
+  return new Promise((resolve, reject) => {
+    s3.upload(params, function (err, data) {
+      if (err) {
+        resolve({
+          statusCode: 500,
+          error: err,
+        });
+        return;
+      }
+      resolve({
+        statusCode: 200,
+        data: data.Location,
+      });
+      console.log(`File uploaded successfully. ${data.Location}`);
+    });
+  });
+};
 
-module.exports = {addMessage, getallMessage, getMsgHistory, getUploadFile, getUploadFileUrl}
+// const get_secure_url = (fileName) => {
+//   return new Promise((resolve, reject) => {
+//     s3.getSignedUrl('getObject', {
+//       Bucket: process.env.AWS_BUCKETNAME,
+//       Key: fileName.indexOf('s3.amazonaws.com') > -1 ? encodeURI(fileName.split('.s3.amazonaws.com/')[1]) : fileName,
+//       Expires: 60 * 5, // time in seconds: e.g. 60 * 5 = 5 mins
+//     }, (err, url) => {
+//       if (err) throw err;
+//       resolve(url);
+//     });
+//   });
+// }
+
+module.exports = {
+  addMessage,
+  getallMessage,
+  getMsgHistory,
+  getUploadFile,
+  getUploadFileUrl,
+};
--- backend/index.js
+++ backend/index.js
@@ -8,24 +8,26 @@ const cookieParser = require("cookie-parser");
 const bodyParser = require("body-parser");
 const fileUpload = require("express-fileupload");
 const bcrypt = require("bcrypt");
+const path = require("path");
 
 const app = express();
-require("dotenv").config()
+require("dotenv").config();
 
 const corsOptions = {
-  origin: 'http://localhost:3000',
-  methods: ['GET', 'POST', 'PUT', 'DELETE'],
-  allowedHeaders: ['Content-Type']
+  origin: "http://localhost:3000",
+  methods: ["GET", "POST", "PUT", "DELETE"],
+  allowedHeaders: ["Content-Type"],
 };
 
-app.use(bodyParser.json())
-app.use(fileUpload())
+app.use(bodyParser.json());
+app.use(fileUpload());
 app.use(cors(corsOptions));
-app.use(express.json())
-app.use(cookieParser())
+app.use(express.json());
+app.use(cookieParser());
+app.use("/uploads", express.static(path.join(__dirname, "uploads")));
 
-app.use("/api/auth",userRoutes);
-app.use("/api/messages",messageRoutes);
+app.use("/api/auth", userRoutes);
+app.use("/api/messages", messageRoutes);
 
 mongoose
   .connect(process.env.MONGO_URL, {
@@ -40,42 +42,39 @@ mongoose
   });
 
 const server = app.listen(process.env.PORT, () => {
-    console.log(`Listening on port ${process.env.PORT}`);
-})
-
+  console.log(`Listening on port ${process.env.PORT}`);
+});
 
 const io = socket(server, {
-  cors:{
-    origin: 'http://localhost:3000',
-    methods: ['GET', 'POST'],
-    allowedHeaders: ['Content-Type'],
-    credentials: true
+  cors: {
+    origin: "http://localhost:3000",
+    methods: ["GET", "POST"],
+    allowedHeaders: ["Content-Type"],
+    credentials: true,
   },
 });
 
 global.onlineUsers = new Map();
 
-io.on("connection",(socket) => {
+io.on("connection", (socket) => {
   global.chatSocket = socket;
-  socket.on("add-user", (userId)=>{
+  socket.on("add-user", (userId) => {
     onlineUsers.set(userId, socket.id);
-  })
+  });
 
-  socket.on("send-msg",(data)=>{
-    const sendUserSocket = onlineUsers.get(data.to)
-    if(sendUserSocket){
-        socket.to(sendUserSocket).emit("msg-recieve", data.message) 
+  socket.on("send-msg", (data) => {
+    const sendUserSocket = onlineUsers.get(data.to);
+    if (sendUserSocket) {
+      socket.to(sendUserSocket).emit("msg-recieve", data.message);
     }
     console.log("Msg1", data.message);
-     
-  })
-  
-  socket.on("send-file",(data)=>{
-    const sendUserSocket = onlineUsers.get(data.to)
-    if(sendUserSocket){
-        socket.to(sendUserSocket).emit("msg-recieve", data.Url) 
-    }
-     console.log(data.Url);
-  })
-})
+  });
 
+  socket.on("send-file", (data) => {
+    const sendUserSocket = onlineUsers.get(data.to);
+    if (sendUserSocket) {
+      socket.to(sendUserSocket).emit("msg-recieve", data.Url);
+    }
+    console.log(data.Url);
+  });
+});
